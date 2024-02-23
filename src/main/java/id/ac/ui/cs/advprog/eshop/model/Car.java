033a7e92147e2d4d79f2cc3eeb3047d887e61d6e
SyauqiArman <syauqiarmanaya@gmail.com>
2024-02-23 09:22:14 +0700

    edit Car.java

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Car.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Car.java
@@ -2,11 +2,9 @@ package id.ac.ui.cs.advprog.eshop.model;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter 
-@Setter
-
+@Getter @Setter
 public class Car {
-    private String carld;
+    private String carId;
     private String carName;
     private String carColor;
     private int carQuantity;
