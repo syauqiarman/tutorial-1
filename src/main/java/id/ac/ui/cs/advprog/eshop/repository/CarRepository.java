64c841a87626fa306819b8521e5b083a89bf69dc
SyauqiArman <syauqiarmanaya@gmail.com>
2024-02-24 16:45:10 +0700

    changing loop to findById method

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/CarRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/CarRepository.java
@@ -34,17 +34,12 @@ public class CarRepository {
     }
 
     public Car update(String id, Car updatedCar) {
-        for (int i = 0; i < carData.size(); i++) {
-            Car car = carData.get(i);
-            if (car.getCarId().equals(id)) {
-                // Update the existing car with the new information
-                car.setCarName(updatedCar.getCarName());
-                car.setCarColor(updatedCar.getCarColor());
-                car.setCarQuantity(updatedCar.getCarQuantity());
-                return car;
-            }
-        }
-        return null; // Handle the case where the car is not found
+        Car car = findById(id);
+        // Update the existing car with the new information
+        car.setCarName(updatedCar.getCarName());
+        car.setCarColor(updatedCar.getCarColor());
+        car.setCarQuantity(updatedCar.getCarQuantity());
+        return car;
     }
     public void delete(String id) {carData.removeIf(car -> car.getCarId().equals(id));}
 }
